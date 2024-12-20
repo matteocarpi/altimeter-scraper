02c6a92cb3a0bced3168a893c6d17cf3b5e7444d
Matteo Carpi <matteocarpi94@gmail.com>
2024-12-20 15:52:00 +0100

    feat: output to json

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+import json
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
@@ -12,17 +13,16 @@ card_list = soup.select('.listing-card')
 
 mapped_cards = []
 
-class ListingItem:
-    # Constructor to initialize the object
-    def __init__(self, title, link):
-        self.title = title
-        self.link = link
-
 for card in card_list:
     link_tag = card.select_one('.listing-thumb')
     url = link_tag.get('href')
     title = link_tag.get('title')
-    mapped_cards.append(card)
+    listing_item = {'title':title, 'url': url}
+    
+    mapped_cards.append(listing_item)
 
-    result = ListingItem(title, url)
-    print(result.title)
+# Export the array to a JSON file
+with open("output.json", "w") as json_file:
+    json.dump(mapped_cards, json_file, indent=4)  # Use `indent` for pretty printing
+
+print("Array exported to output.json")
