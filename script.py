81af6d53ae2caed2ac63566136305cb7c2c52500
Matteo Carpi <matteocarpi94@gmail.com>
2024-12-20 18:18:38 +0100

    feat: print nice links to the console

--- script.py
+++ script.py
@@ -2,6 +2,11 @@ import json
 from datetime import datetime
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+import os
+
+# Get the absolute path of the current script
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
 
 source_url = "https://soaring.de/osclass/index.php?page=search&sOrder=dt_pub_date&iOrderType=desc&sPattern=altimeter+57&sPriceMax=1000"
 
@@ -13,7 +18,9 @@ soup = BeautifulSoup(html, "html.parser")
 
 card_list = soup.select('.listing-card')
 
-with open('listing-archive.json', 'r') as json_file:
+listing_archive_path =  os.path.join(script_dir, 'listing-archive.json')
+
+with open(listing_archive_path, 'r') as json_file:
     listing_archive = json.load(json_file)
 
 current_items = []
@@ -31,7 +38,7 @@ today = datetime.today().strftime('%Y-%m-%d')
 listing_archive[today] = current_items
 
 # Export the array to a JSON file
-with open("listing-archive.json", "w") as json_file:
+with open(listing_archive_path, "w") as json_file:
     json.dump(listing_archive, json_file, indent=4)
 
 archive_properties_list = list(listing_archive.keys())
@@ -45,8 +52,15 @@ previous_listings = listing_archive[prevous_listings_key]
 # Array of urls 
 previous_urls = {listing["url"] for listing in previous_listings}
 
-new_items = [listing for listing in current_items if listing["url"] not in previous_urls]
+new_listings = [listing for listing in current_items if listing["url"] not in previous_urls]
+    
+def create_hyperlink(url, label):
+    return f"\033]8;;{url}\033\\{label}\033]8;;\033\\"
 
-print(json.dumps(new_items, indent = 4))
+# Print the title
+print("\n    Here's what's new:\n")
 
-# Import list and compare it with new one
+# Print each item with a clickable Markdown link
+for listing in new_listings:
+    # print(f"{listing['title']}: {listing['url']}\n")
+    print(f"     - {create_hyperlink(listing['url'], listing['title'])}\n")
